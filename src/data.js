18778ab1cc81911dc8b3af4c58e314d36596f008
christian sosa <csosa@arkho.io>
2024-12-08 19:50:52 -0300

    fecha 8

--- src/data.js
+++ src/data.js
@@ -1,17 +1,13 @@
 export const initialTeams = [
-  { id: 1, name: "ParqueRoma", points: 23, played: 9, color: "#FF5733" },
+  { id: 1, name: "ParqueRoma", points: 26, played: 10, color: "#FF5733" },
   { id: 2, name: "Manchester", points: 18, played: 8, color: "#33FF57" },
-  { id: 3, name: "Rimini", points: 18, played: 8, color: "#3357FF" },
-  { id: 4, name: "Molino", points: 21, played: 8, color: "#F1C40F" },
+  { id: 3, name: "Rimini", points: 18, played: 9, color: "#3357FF" },
+  { id: 4, name: "Molino", points: 22, played: 9, color: "#F1C40F" },
   { id: 5, name: "LaLosa", points: 16, played: 8, color: "#9B59B6" },
   { id: 6, name: "Lucaneta", points: 12, played: 7, color: "#E74C3C" },
 ];
   
   export const initialFixture = [
-    { id: 1, team1: "Molino", team2: "Tuttocuoio", result: null, date: "Fecha 8"},
-    { id: 2, team1: "ParqueRoma", team2: "Rimini", result: null,  date: "Fecha 8"},
-    { id: 3, team1: "Lucaneta", team2: "Wittemberg", result: null,  date: "Fecha 8"},
-    { id: 4, team1: "LaLosa", team2: "PocaBrasa", result: null,  date: "Fecha 8"},
     { id: 5, team1: "Manchester", team2: "Lucaneta", result: null,  date: "Fecha 9"},
     { id: 6, team1: "ParqueRoma", team2: "Molino", result: null,  date: "Fecha 9"},
     { id: 7, team1: "LaLosa", team2: "Wittemberg", result: null,  date: "Fecha 10"},
